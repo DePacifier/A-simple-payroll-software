9276b8d31c5bbc6ad15cbcde98c93a3a5657a493
DePacifier <milkybekele@gmail.com>
2021-01-05 06:18:02 +0300

    Payroll software started with a console application format

---
+++ A Simple Payroll Software/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A_Simple_Payroll_Software
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
